aa1c64052de013e7ff51aeb5d32e4c2deff0347c
Lucas Jiao <105329087+lLukii@users.noreply.github.com>
2023-11-24 12:25:53 -0500

    Update generator.py

--- Final Project/generator.py
+++ Final Project/generator.py
@@ -1,8 +1,6 @@
 # Algorithms that control the game logic are written in this file. 
 
 from random import shuffle, randint
-from json import dumps
-
 
 
 def check(i, j, board):
@@ -78,5 +76,4 @@ def main():
 
 
 
-testboard = [[5, 6, 2, 4, 4, 0, 3, 0, 8], [1, 3, 4, 0, 5, 0, 6, 0, 0], [0, 8, 9, 3, 4, 6, 1, 0, 0], [2, 1, 3, 0, 0, 5, 8, 0, 0], [0, 0, 6, 8, 0, 7, 0, 0, 0], [8, 0, 7, 1, 2, 3, 4, 0, 6], [0, 0, 0, 6, 0, 1, 9, 8, 4], [0, 7, 1, 9, 0, 0, 5, 0, 2], [9, 4, 8, 0, 3, 2, 0, 6, 1]]
-print(check(0, 4, testboard))
+
