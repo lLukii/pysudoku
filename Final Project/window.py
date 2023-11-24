a5da0ea71afa3a7b151272313ecb4d5a869da598
Lucas Jiao <105329087+lLukii@users.noreply.github.com>
2023-11-24 12:25:12 -0500

    Update window.py

--- Final Project/window.py
+++ Final Project/window.py
@@ -4,6 +4,9 @@
 # Module imports
 import pygame
 import generator
+from time import perf_counter
+from pathlib import Path
+import recordscore
 
 # Pygame window setup
 pygame.init()
@@ -12,6 +15,7 @@ pygame.display.set_caption("Pysudoku")
 clock = pygame.time.Clock()
 
 white = (255,255,255)
+grey = (210,210,210)
 black = (0,0,0)
 red = (255,0,0)
 
@@ -26,10 +30,11 @@ tiles = []
 num_pos = []
 answer_board = []
 player_board = [] # initial layout board given to player
-dynamic_board = [] # dynamic board that keeps track of current player progress
+dynamic_board = []
+errors = [[False for i in range(9)] for i in range(9)] # Checks if the current tile number is valid based on the situation of the board. 
+
 io = False
 inited = False
-mistakes = False
 ind_i, ind_j = 0, 0 # used for tracking which num_pos event.key needs to fill
 TILE_SIZE = 60
 TILE_ORIGIN = 120 # 720/8
@@ -41,10 +46,30 @@ def generate_title(text, center):
     titlehitbox.center = center
     screen.blit(title, titlehitbox)
 
+def check_win():
+    nonzero = 0
+    for row in dynamic_board:
+        for num in row:
+            if num != 0:
+                nonzero += 1
+    
+    if nonzero == 81:
+        trues = 0
+        for row in errors:
+            for err in row:
+                if not err:
+                    trues += 1
+        
+        if trues == nonzero:
+            return True
+    
+    return False
+
+
 # Pygame mainloop
 gameOn = True
-tck = 0
-time = 0
+start_time, end_time = 0, 0
+
 while gameOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,23 +79,31 @@ while gameOn:
             if event.key > pygame.K_0 and event.key <= pygame.K_9:
                 overriderect = pygame.Rect(num_pos[9*ind_i + ind_j][0]-TILE_SIZE/3, num_pos[9*ind_i + ind_j][1]-TILE_SIZE/3, 2*TILE_SIZE/3, 2*TILE_SIZE/3)
                 pygame.draw.rect(screen, black, overriderect)
+                # update board position with the pressed number, and override the number on pygame window. 
                 dynamic_board[ind_i][ind_j] = event.key-48
                 # check if placed tile is valid
                 num = 0
                 overriderect = pygame.Rect(0, 0, 720, 100)
                 pygame.draw.rect(screen, black, overriderect)
                 if generator.check(ind_i, ind_j, dynamic_board):
-                    num = nums.render(str(event.key-48), True, white)
+                    num = nums.render(str(event.key-48), True, grey)
                     generate_title("Everything looks good!", (250, 50))
+                    errors[ind_i][ind_j] = False
                 
                 else:
                     num = nums.render(str(event.key-48), True, red)
-                    generate_title(f"Oops! You have a mistake at row {ind_i+1} column {ind_j+1}", (400, 50))
-                    mistakes = True
+                    generate_title(f"Oops! You have a mistake at row {ind_i+1} column {ind_j+1}", (400, 50))   
+                    errors[ind_i][ind_j] = True
                 
                 numbox = num.get_rect()
                 numbox.center = num_pos[9*ind_i + ind_j]
                 screen.blit(num, numbox)
+
+                if check_win():
+                    gamestate = "VICTORY"
+                    screen.fill(black)
+                    io = False
+                    inited = False
 
     if gamestate == "TITLE_SCREEN":
         title = heading.render("Welcome to pysudoku!", True, white)
@@ -90,15 +123,12 @@ while gameOn:
             mpos = pygame.mouse.get_pos()
             if playGame.collidepoint(mpos):
                 gamestate = "GAMEPLAY"
-                screen.fill((0,0,0))
+                screen.fill(black)
         
     elif gamestate == "GAMEPLAY":
-        tck += 1
-        if tck == 24:
-            tck = 0
-            time += 1
         
         if not inited:
+            start_time = perf_counter()
             for i in range(9):
                 row = []
                 for j in range(9):
@@ -106,7 +136,6 @@ while gameOn:
                     pygame.draw.rect(screen, white, rect, 2)
                     row.append(rect)
                 tiles.append(row)
-            
             
             answer_board = generator.main()
             # creating initial player board
@@ -132,6 +161,7 @@ while gameOn:
             
             # displaying defualt text
             generate_title("Make your first move: ", (250, 50))
+            print(answer_board)
             inited = True
 
         # set the position in which the number is placed
@@ -144,27 +174,21 @@ while gameOn:
                         io = True
                         ind_i = i
                         ind_j = j
-
+        
+    elif gamestate == "VICTORY":  
+        if not inited: 
+            end_time = perf_counter()
+            title = heading.render("Congratulations!\n You completed the puzzle!", True, white)
+            titlehitbox = title.get_rect()
+            titlehitbox.center = (360, 75)
+            screen.blit(title, titlehitbox)
+            highscore = recordscore.file_io(end_time-start_time)
+            generate_title(f"This round: {end_time-start_time} seconds", (300,300))
+            generate_title(f"Your best: {highscore} seconds", (200,200))
+            inited = True
 
     pygame.display.update() 
     clock.tick(24)
-
-outtime = ""
-if time//60 < 10:
-    outtime += "0" + str(time//60)
-
-else:
-    outtime += str(time//60)
-
-outtime += ":"
-if time%60 < 10:
-    outtime += "0" + str(time%60)
-
-else:
-    outtime += str(time%60)
-
-print(outtime)
-
 
         
 
